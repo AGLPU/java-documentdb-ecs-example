f461049d136d5f0b8d5245c6537d09cd8bcc24c6
aman.goel1 <aman.goel1@ihsmarkit.com>
2025-11-23 00:25:58 +0530

    github actions

--- src/main/java/com/palmer/rachelle/javaspringmongodb/MongoClientProvider.java
+++ src/main/java/com/palmer/rachelle/javaspringmongodb/MongoClientProvider.java
@@ -6,33 +6,15 @@ import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
-import java.security.KeyStore;
 import java.util.Base64;
 
 public class MongoClientProvider {
 
     public static MongoClient createSslClient() throws Exception {
-        // Load your CA certificate into a Java truststore (JKS)
-        String trustStorePath = "C:/Learning/Projects/Usersaman.goel1docdb-truststore.jks";
-        char[] trustStorePassword = "changeit".toCharArray();
-
-        KeyStore ts = KeyStore.getInstance("JKS");
-        try (FileInputStream fis = new FileInputStream(trustStorePath)) {
-            ts.load(fis, trustStorePassword);
-        }
-
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        tmf.init(ts);
 
         String base64 = "PjNIcllwcG0+Uk9bMTlkZFdiUls1WExwM01wLQ==";
         String decodedPassword = new String(Base64.getDecoder().decode(base64));
 
-        // Create an SSLContext using this trust store
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, tmf.getTrustManagers(), null);
         MongoCredential credential = MongoCredential.createCredential(
                 "aglpu",
                 "BookStore",
@@ -41,12 +23,12 @@ public class MongoClientProvider {
 
         // Build the MongoClientSettings
         ConnectionString connString = new ConnectionString(
-            "mongodb://localhost:27017/?tls=true&retryWrites=false"
-        );
+                System.getenv("MONGO_URI")
+         );
         MongoClientSettings settings = MongoClientSettings.builder()
                 .credential(credential)
             .applyConnectionString(connString)
-            .applyToSslSettings(ssl -> ssl.enabled(true).context(sslContext).invalidHostNameAllowed(true))
+            .applyToSslSettings(ssl -> ssl.enabled(true).invalidHostNameAllowed(true))
             .build();
 
         return MongoClients.create(settings);

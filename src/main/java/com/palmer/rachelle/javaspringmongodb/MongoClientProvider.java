cf9ceff7a9f5147ba1b7ed5f953b7d7b2ca5af1b
aman.goel1 <aman.goel1@ihsmarkit.com>
2025-12-01 22:25:16 +0530

    logs

--- src/main/java/com/palmer/rachelle/javaspringmongodb/MongoClientProvider.java
+++ src/main/java/com/palmer/rachelle/javaspringmongodb/MongoClientProvider.java
@@ -5,20 +5,24 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Base64;
 
 public class MongoClientProvider {
 
+    private static final Logger log = LoggerFactory.getLogger(MongoClientProvider.class);
+
     public static MongoClient createSslClient() throws Exception {
 
-        String base64 = "PjNIcllwcG0+Uk9bMTlkZFdiUls1WExwM01wLQ==";
+       // String base64 = "PjNIcllwcG0+Uk9bMTlkZFdiUls1WExwM01wLQ==";
        // String decodedPassword = new String(Base64.getDecoder().decode(base64));
         var mongoUri=System.getenv("MONGO_URI");
         var password=System.getenv("MONGO_PASSWORD");
         var dbNme=System.getenv("MONGO_DBNAME");
 
-
+        log.info("mongoUri:{}, password:{}, dbNme:{}",mongoUri,password,dbNme);
         MongoCredential credential = MongoCredential.createCredential(
                 "aglpu",
                 dbNme,
@@ -29,6 +33,8 @@ public class MongoClientProvider {
         ConnectionString connString = new ConnectionString(
                 System.getenv(mongoUri)
          );
+        log.info("connString:{}",connString);
+
         MongoClientSettings settings = MongoClientSettings.builder()
                 .credential(credential)
             .applyConnectionString(connString)
